4661664fe5a54ac3ad7db81a44549d96a525ae46
P_essonam <essonammaximin@gmail.com>
2023-12-07 19:03:46 +0100

    save state of previous page and send it to the next via link > header and dashoboard page

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+
+  const data = 3
   return (
     <header>
         <h1>Header</h1>
-        <NavLink className={({isActive}) => {return (isActive ? "my-link": null)}} to='/post'>Newpost</NavLink>
+        <NavLink className={({isActive}) => {return (isActive ? "my-link": null)}} to='/post' state={data}>Newpost</NavLink>
     </header>
   )
 }
--- src/Layout.js
+++ src/Layout.js
@@ -3,13 +3,17 @@ import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 import Nav from './Nav'
+import { useSearchParams } from 'react-router-dom'
 
 const Layout = () => {
+
+  const [searchParams, setSearchparams] = useSearchParams()
   return (
     <> 
         <Header />
         <Nav />
         <Outlet/>
+        {console.log(searchParams.get('type'))}
         <Footer />
     </>
   )
